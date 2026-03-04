279f00a138cec717cf2288f2d230abf5ef78ad3c
João Gabriel <joaogabriel9633@gmail.com>
2026-03-04 13:20:17 -0300

    feat

--- shared/colors.ts
+++ shared/colors.ts
@@ -1,6 +1,6 @@
 export const colors = {
   "button-primary": "#6E6159",
-  "button-secondary": "#8F8176",
+  "button-secondary": "#564b42",
   "text-primary": "#665C54",
   "text-secondary": "#665C54",
   "border-input-primary": "#AB9C91",
